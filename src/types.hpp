289eff1ec7367988fc6e5b57dead2672e9555006
Francesco Boffa <fra.boffa@gmail.com>
2018-11-24 10:36:16 +0000

    Add shorthand types

---
+++ src/types.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
