9757e72828d13fa616b9e0bb804912c9f4f30df8
Francesco Boffa <fra.boffa@gmail.com>
2018-11-24 10:38:46 +0000

    Start the memory and load a bios rom

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include "Memory.hpp"
+#include "types.hpp"
+
 using namespace std;
 
 int main() {
-	cout << "Hello world" << endl;
+    Memory memory;
+
+    memory.loadBios("SCPH1001.BIN");
+
 	return 0;
 }
